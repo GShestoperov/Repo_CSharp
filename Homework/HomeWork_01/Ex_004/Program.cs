7ca71a26aeba446fc3e93b039b623622c45a1f24
Georgy <gshestoperov@gmail.com>
2023-03-06 14:21:19 +0300

    Add Homework_01/Ex_004

---
+++ Homework/HomeWork_01/Ex_004/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a, b, c, max;
+Console.Write("Введите три числа: ");
+while (!int.TryParse(Console.ReadLine(), out a)) ;
+while (!int.TryParse(Console.ReadLine(), out b)) ;
+while (!int.TryParse(Console.ReadLine(), out c)) ;
+
+if (a > b)
+    max = a;
+else
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine("Max = " + max);
--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 - Homework
     - HomeWork_01
         - Ex_002: [Блок-схема](Homework/HomeWork_01/Ex_002/diagram.png) >>>  [Код](Homework/HomeWork_01/Ex_002/Program.cs) 
-        - Exp_004:
+        - Ex_004: [Блок-схема](Homework/HomeWork_01/Ex_004/diagram.png) >>>  [Код](Homework/HomeWork_01/Ex_004/Program.cs) 
 
 - Lecture
     - Lecture_01
