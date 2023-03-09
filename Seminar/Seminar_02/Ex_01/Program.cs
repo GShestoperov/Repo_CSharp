6d423bc58460e21e0f0be5bd62548b87d8bf7ff6
Georgy <gshestoperov@gmail.com>
2023-03-09 17:51:54 +0300

    Add Seminar_02/Ex_01

---
+++ Seminar/Seminar_02/Ex_01/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[] array = { 10, 4, 6, 5, 20, 36, 40 };
+int index;
+int[] newArray = new int[array.Length];
+int newIndex = 0;
+
+for (index = 0; index < array.Length; index++)
+{
+    if (array[index] % 5 == 0)
+    {
+        newArray[newIndex] = array[index];
+        newIndex++;
+    }
+}
+
+for (newIndex = 0; newIndex < newArray.Length; newIndex++)
+{
+    Console.Write(newArray[newIndex] + " ");
+}
