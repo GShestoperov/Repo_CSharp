c785da3e9a7c518d82909a98913a09015dc60a09
Georgy <gshestoperov@gmail.com>
2023-03-11 16:23:24 +0300

    Add Homework_04/Task_29

---
+++ Homework/HomeWork_04/Task_29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] CreateArray(int length)
+{
+    return new int[length];
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = CreateArray(8);
+FillArray(array);
+PrintArray(array);
